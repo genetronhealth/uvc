c1a5a1ca663db800ac0f23eab2bfced8fa2aec4d
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2019-05-14 23:38:04 +0800

    Added the wrapper script uvcTN.sh which runs tumor-normal comparison (cmdline params are : ref fasta, tumor bam, normal bam, outdir, sample name)

---
+++ uvcTN.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+set -ex
+
+ref="$1"
+tbam="$2"
+nbam="$3"
+outdir="$4"
+samplename="$5"
+
+scriptdir="$(dirname "$(which "$0")")"
+
+tvcfgz="${outdir}/${samplename}_T_uvc1.vcf.gz"
+nvcfgz="${outdir}/${samplename}_N_uvc1.vcf.gz"
+avcfgz="${outdir}/${samplename}_TandN_uvc1.vcf.gz"
+
+
+tlog="${outdir}/${samplename}_T_uvc1.stderr"
+nlog="${outdir}/${samplename}_N_uvc1.stderr"
+
+mkdir -p "${outdir}"
+
+date && time -p "${scriptdir}/uvc1" -t 16 -f "${ref}" -s "${samplename}_T" "${tbam}" -o "${tvcfgz}" 2> "${tlog}"
+date && time -p "${scriptdir}/uvc1" -t 16 -f "${ref}" -s "${samplename}_N" "${nbam}" -o "${nvcfgz}" 2> "${nlog}"
+
+date && time -p bcftools index -t "${tvcfgz}"
+date && time -p bcftools index -t "${nvcfgz}"
+
+date && time -p bftools merge -m none "${tvcfgz}" "${nvcfgz}" | "${scriptdir}/callTN1" - "${avcfgz}"
+date && time -p bcftools index -t "${avcfgz}"
+
