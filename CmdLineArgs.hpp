63630718c46340fbadc67f79aa2c3a6c32eca7b9
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2019-05-22 09:38:35 +0800

    Added cmd line options from decomposed CORRECTION_ ... values.

--- CmdLineArgs.hpp
+++ CmdLineArgs.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include "CLI11-1.7.1/CLI11.hpp"
+#include "common.h"
 
 struct CommandLineArgs {
     std::string bam_input_fname = ""; // not missing
@@ -12,6 +13,12 @@ struct CommandLineArgs {
     std::string bed_region_fname = "";
     std::string sample_name = "-";
     std::string tsv_primer_fname = "";
+    
+    bool is_dup_aware = true;
+    AssayType assay_type = ASSAY_TYPE_AUTO;
+    MoleculeTag molecule_tag = MOLECULE_TAG_AUTO;
+    SequencingPlatform sequencing_platform = SEQUENCING_PLATFORM_AUTO;
+    PairEndMerge pair_end_merge = PAIR_END_MERGE_YES;
     // https://www.biostars.org/p/110670/
     uint32_t    min_depth_thres = 4;
     uint32_t    min_altdp_thres = 2;
