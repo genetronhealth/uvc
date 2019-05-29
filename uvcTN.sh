83b3a6105df299ffe978e545811ee567c04347ef
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2019-05-29 18:37:57 +0800

    reordered statements in uvcTN.sh

--- uvcTN.sh
+++ uvcTN.sh
@@ -19,9 +19,9 @@ nlog="${outdir}/${samplename}_N_uvc1.stderr"
 mkdir -p "${outdir}"
 
 date && time -p "${scriptdir}/uvc1" -f "${ref}" -s "${samplename}_T" "${tbam}" -o "${tvcfgz}" "${@:6}" 2> "${tlog}"
-date && time -p "${scriptdir}/uvc1" -f "${ref}" -s "${samplename}_N" "${nbam}" -o "${nvcfgz}" "${@:6}" --tumor-vcf "${tvcfgz}" 2> "${nlog}"
-
 date && time -p bcftools index -t "${tvcfgz}"
+
+date && time -p "${scriptdir}/uvc1" -f "${ref}" -s "${samplename}_N" "${nbam}" -o "${nvcfgz}" "${@:6}" --tumor-vcf "${tvcfgz}" 2> "${nlog}"
 date && time -p bcftools index -t "${nvcfgz}"
 
 date && time -p bcftools merge -m none -Ou "${tvcfgz}" "${nvcfgz}" | "${scriptdir}/callTN1" - "${avcfgz}"
