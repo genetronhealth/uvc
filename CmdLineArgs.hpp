b5d866eff7c5e2d545939fed2524ca558df0aaaa
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2019-06-30 16:55:50 +0800

    Changed default value of nonref_to_alt_frac_indel to improve sensitivity a little bit.

--- CmdLineArgs.hpp
+++ CmdLineArgs.hpp
@@ -45,8 +45,8 @@ struct CommandLineArgs {
     uint32_t    bq_phred_added_misma = 0;
     bool        should_add_note = false;
     uint32_t    phred_germline_polymorphism = 30;
-    double      nonref_to_alt_frac_snv = 0.001;
-    double      nonref_to_alt_frac_indel = 0.25;
+    double      nonref_to_alt_frac_snv = 0.001; // 0.50 for practically removing tri-allelic sites.
+    double      nonref_to_alt_frac_indel = 0.20;
     double      tnq_mult_snv = 1.0;
     double      tnq_mult_indel = 2.0;
     int initFromArgCV(int & parsing_result_flag, SequencingPlatform & inferred_sequencing_platform, int argc, const char *const* argv);
