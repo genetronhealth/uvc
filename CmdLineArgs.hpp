a33932a3febdbd8ca8d7bbf2c47090992ad4357b
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2019-07-05 15:20:01 +0800

    Removed the constant 2 in to_phred_like of Any4. Changed the default params accordingly.

--- CmdLineArgs.hpp
+++ CmdLineArgs.hpp
@@ -50,10 +50,10 @@ struct CommandLineArgs {
     uint32_t    bq_phred_added_misma = 0;
     bool        should_add_note = false;
     uint32_t    phred_germline_polymorphism = 30;
-    double      nonref_to_alt_frac_snv = 0.35; // 0.50 for practically removing tri-allelic sites.
-    double      nonref_to_alt_frac_indel = 0.20;
+    double      nonref_to_alt_frac_snv = 0.30; // 0.50 for practically removing tri-allelic sites.
+    double      nonref_to_alt_frac_indel = 0.15;
     double      tnq_mult_snv = 1.0;
-    double      tnq_mult_indel = 2.0;
+    double      tnq_mult_indel = 2.0; // maybe 3.0
     int initFromArgCV(int & parsing_result_flag, SequencingPlatform & inferred_sequencing_platform, int argc, const char *const* argv);
     SequencingPlatform selfUpdateByPlatform(void);
 };
