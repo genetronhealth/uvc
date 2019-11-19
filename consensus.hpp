03bdc3f67013e197629db97685834ce88e77e924
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2019-11-20 00:01:03 +0800

    minor tweak with qminus (AD factor from 10 to 4). Give more weight to tnq_onlyT by using it in tnq_TandN. Removed calc_dim_return in tnq's.

--- consensus.hpp
+++ consensus.hpp
@@ -2313,7 +2313,7 @@ appendVcfRecord(std::string & out_string, std::string & out_string_pass, const S
             double tFA1 = (tAD1 / (DBL_EPSILON + (double)tDP1));
             double nFA1 = (nAD1 / (DBL_EPSILON + (double)nDP1));
             double ratioFA1 = nFA1 / (nFA1 + tFA1 + DBL_EPSILON); 
-            double qminus = ratioFA1 * ratioFA1 * 9.0 * MIN(10.0, tAD1 * nDP1 / ((double)tDP1 + DBL_EPSILON));
+            double qminus = ratioFA1 * ratioFA1 * 9.0 * MIN(4.0, tAD1 * nDP1 / ((double)tDP1 + DBL_EPSILON));
             tnlike_alt -= qminus;
         }
         double nRD1 = (nUseHD ? (highqual_thres * (fmt.cRefHD[0] + fmt.cRefHD[1])) : (fmt.cRefBQ[0] + fmt.cRefBQ[1]));
@@ -2329,16 +2329,14 @@ appendVcfRecord(std::string & out_string, std::string & out_string_pass, const S
                 ? ((double)((int)phred_germline - (int)fmt.GQ))
                 : ((double)(     phred_germline +      fmt.GQ)));
         
+        // // calc_dim_return((double)tvn_vaq, (double)300, (double)2),
         // double effectiveFA = calc_upper_bounded(tki.FA, 0.5, 0.1); // effectively cap the value att 0.1
+        double tvq_ubmax = calc_upper_bounded(log(tki.FA + DBL_EPSILON) / log(10.0) * (10.0 * 2.0) + 80.0, (double)57, (double)60);
         auto tvn_vaq = MIN(tnlike_alt, tnlike_nonref);
-        double tnq_TandN = MIN(calc_dim_return((double)tvn_vaq, (double)30, (double)2),
-                calc_upper_bounded(log(tki.FA + DBL_EPSILON) / log(10.0) * (10.0 * 2.0) + 80.0, (double)57, (double)60)) 
-                    - (1.0 / MAX(10.0, (double)tvn_vaq));
-        double tnq_onlyT = MIN(calc_dim_return((double)tki.VAQ, (double)30, (double)2),
-                calc_upper_bounded(log(tki.FA + DBL_EPSILON) / log(10.0) * (10.0 * 2.0) + 80.0, (double)57, (double)60)) 
-                    - (1.0 / MAX(10.0, (double)tki.VAQ)); // truncate tumor VAQ
+        double tnq_onlyT = MIN((double)tki.VAQ, tvq_ubmax) - (1.0 / MAX(10.0, (double)tki.VAQ)); // truncate tumor VAQ
+        double tnq_TandN = MIN((double)tvn_vaq, tnq_onlyT) - (1.0 / MAX(10.0, (double)tvn_vaq));
         double tnq_mult = (isInDel ? tnq_mult_indel : tnq_mult_snv);
-        double tnq_val = tnq_TandN * tnq_mult + tnq_onlyT;
+        double tnq_val = tnq_onlyT + (tnq_TandN * tnq_mult);
         vcfqual = MIN(tnq_val, phred_non_germ);
         
         infostring += std::string(";TNQ=") + std::to_string(tnlike_alt);
