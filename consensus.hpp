19fdf87c4e63420325e7b1c79e691b856560fdad
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2019-11-20 17:58:32 +0800

    Apply minus 5 to VCF QUAL for each tumor AD below the depth threshold of 4.

--- consensus.hpp
+++ consensus.hpp
@@ -2474,9 +2474,10 @@ appendVcfRecord(std::string & out_string, std::string & out_string_pass, const S
                 // }
                 
                 if (tAD < 3.5) {
-                    double tADf = (double)tAD;
-                    double tbase = 1.0 / (double)(tADf < 1.5 ? 1 : (tADf < 2.5 ? 4 : 16));
-                    vcfqual = vcfqual * tAD / (tAD + tbase);
+                    vcfqual -= (double)(5 * (4 - tAD));
+                    //double tADf = (double)tAD;
+                    //double tbase = 1.0 / (double)(tADf < 1.5 ? 1 : (tADf < 2.5 ? 4 : 16));
+                    //vcfqual = vcfqual * tAD / (tAD + tbase);
                     //vcfqual *= (tAD / 1.5);
                 }
                 // micro-adjustment for LOD // WARNING: no theoretical justification
