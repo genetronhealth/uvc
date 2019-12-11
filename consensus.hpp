98634a553b7d521da2f183d12dc432850231c4f2
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2019-12-11 19:33:13 +0800

    Increased contam rate from 0.02 to 0.05 to give less penalty to germline risk.

--- consensus.hpp
+++ consensus.hpp
@@ -2097,23 +2097,23 @@ fillBySymbol(bcfrec::BcfFormat & fmt, const Symbol2CountCoverageSet & symbol2Cou
             const double fref  = 1.0 - fa;
             // const double nfa   = MAX(0.5, fa);
             // const double nfref = MAX(0.5, fref);
-            const double t2n_contam_rate = 0.02;
+            const double t2n_contam_rate = 0.05;
             
             // Uni-directional deviation from its theoretical distribution is translated into a phred-scaled error probability. TODO: check the effect of sqrt?
-            int hetREF_likelim = -(int)(10.0/log(10.00)*2.5*sqrt(1.0) * MAX(log(0.500 * ref_mulfact / fref), 0.0));       // het-ref to ALT mul error phred
-            int hetALT_likelim = -(int)(10.0/log(10.00)*2.5*sqrt(1.0) * MAX(log(0.500 * ref_mulfact / fa  ), 0.0));       // het-alt to REF mul error phred 
-            int homref_likelim = -(int)(10.0/log(10.00)*2.5*sqrt(1.0) * log(MAX(fa   / t2n_contam_rate,  1.0 / fref)));   // hom-alt to REF mul error phred
-            int homalt_likelim = -(int)(10.0/log(10.00)*2.5*sqrt(1.0) * log(MAX(fref / t2n_contam_rate,  1.0 / fa  )));   // hom-ref to ALT mul error phred
+            int hetREF_likelim = -(int)(10.0/log(10.00)*2.5*2.0 * MAX(log(0.500 * ref_mulfact / fref), 0.0));       // het-ref to ALT mul error phred
+            int hetALT_likelim = -(int)(10.0/log(10.00)*2.5*2.0 * MAX(log(0.500 * ref_mulfact / fa  ), 0.0));       // het-alt to REF mul error phred 
+            int homref_likelim = -(int)(10.0/log(10.00)*2.5*2.0 * log(MAX(fa   / t2n_contam_rate,  1.0 / fref)));   // hom-alt to REF mul error phred
+            int homalt_likelim = -(int)(10.0/log(10.00)*2.5*2.0 * log(MAX(fref / t2n_contam_rate,  1.0 / fa  )));   // hom-ref to ALT mul error phred
             
             const double dref  = fmt.DP * fref;
             //const double nad   = fmt.DP * nfa;
             //const double ndref = fmt.DP * nfref;
             
             // assuming statistical independence of reads, kl-divergence is translated into a phred-scaled error probability.
-            int hetREF_likeval = -(int)calc_binom_10log10_likeratio(0.500 * ref_mulfact, da,   dref );           // het-ref to ALT add error phred
-            int hetALT_likeval = -(int)calc_binom_10log10_likeratio(0.500 * ref_mulfact, dref, da   );           // het-alt to REF add error phred
-            int homref_likeval = -(int)calc_binom_10log10_likeratio(t2n_contam_rate    , da,   dref );           // hom-alt to REF add error phred by contamination
-            int homalt_likeval = -(int)calc_binom_10log10_likeratio(t2n_contam_rate    , dref, da   );           // hom-ref to ALT add error phred by contamination
+            int hetREF_likeval = -(int)calc_binom_10log10_likeratio(0.500 * ref_mulfact, da,   dref );              // het-ref to ALT add error phred
+            int hetALT_likeval = -(int)calc_binom_10log10_likeratio(0.500 * ref_mulfact, dref, da   );              // het-alt to REF add error phred
+            int homref_likeval = -(int)calc_binom_10log10_likeratio(t2n_contam_rate    , da,   dref );              // hom-alt to REF add error phred by contamination
+            int homalt_likeval = -(int)calc_binom_10log10_likeratio(t2n_contam_rate    , dref, da   );              // hom-ref to ALT add error phred by contamination
             
             fmtG8 = {
                     -homref_likelim, -hetREF_likelim, -hetALT_likelim, -homalt_likelim,
