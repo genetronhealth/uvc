19c1e16e429e455419c70dc25c1be915c8e9e307
zhaoxiaofei <xiaofei.zhao@genetronhealth.com>
2022-03-31 11:30:37 +0800

    Checked-in iohts.hpp and iohts.cpp to the code-base.

---
+++ iohts.cpp
@@ -0,0 +1,34 @@
+#include "iohts.hpp"
+#include "common.hpp"
+
+#include "htslib/faidx.h"
+#include "htslib/sam.h"
+#include "htslib/vcf.h"
+
+#include "<vector>"
+
+bool BedLine::is_valid() {
+    return ((tid >= 0) || (tname.size() > 0)) && (beg_pos < end_pos);
+}
+
+std::vector<bam1_t *>
+load_bam_records(
+        const char * bam_fname,
+        const hts_idx_t * hts_idx, 
+        const uvc1_refgpos_t query_tid, 
+        const uvc1_refgpos_t query_beg, 
+        const uvc1_refgpos_t query_end) {
+    
+    std::vector<bam1_t *> ret;
+    samFile *sam_infile = sam_open(bam_fname, "r");
+    bam1_t *aln = bam_init1();
+    hts_itr_t *hts_itr = sam_itr_queryi(hts_idx, query_tid, query_beg, query_end);
+    int itr_result = 0;
+    while ((itr_result = sam_itr_next(sam_infile, hts_itr, aln)) >= 0) { 
+        ret.push_back(bam_dup1(aln));
+    }
+    sam_itr_destroy(hts_itr);
+    bam_destroy1(aln);
+    return ret;
+}
+
---
+++ iohts.hpp
@@ -0,0 +1,46 @@
+#ifndef IS_IOHTS_INCLUDED
+#define IS_IOHTS_INCLUDED
+
+#include "common.hpp"
+
+#include "htslib/sam.h"
+#include "htslib/hts.h"
+
+#include <string>
+#include <vector>
+
+#define BED_END_TO_END_BIT 0x1
+
+struct BedLine {
+    std::string tname; // can be set to empty if tid != -1
+    uvc1_refgpos_t tid; // can be set to -1 if !tname.isEmpty()
+    uvc1_refgpos_t beg_pos;
+    uvc1_refgpos_t end_pos;
+    uvc1_flag_t region_flag;
+    uvc1_readnum_big_t n_reads;
+    
+    BedLine(
+            uvc1_refgpos_t a_tid,
+            uvc1_refgpos_t a_beg_pos,
+            uvc1_refgpos_t a_end_pos,
+            const uvc1_flag_t a_region_flag,
+            const uvc1_readnum_big_t a_n_reads) {
+        this->tid = a_tid;
+        this->beg_pos = a_beg_pos;
+        this->end_pos = a_end_pos;
+        this->region_flag = a_region_flag;
+        this->n_reads = a_n_reads;
+    }; 
+    bool is_valid();
+};
+
+std::vector<bam1_t *>
+load_bam_records(
+        const char * bam_fname,
+        const hts_idx_t * hts_idx,
+        const uvc1_refgpos_t query_tid,
+        const uvc1_refgpos_t query_beg, 
+        const uvc1_refgpos_t query_end);
+
+#endif
+
